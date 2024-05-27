fcf537d70783b11ba2bffdc00d7802a843979d13
eo-uk <london.ekin@gmail.com>
2024-05-27 17:22:16 +0100

    added data

---
+++ data.py
@@ -0,0 +1,43 @@
+TRAINING_DATA = [
+    """
+    Hi,
+
+    Have you tried our amazing ice cream? It is an amazing ice cream.   
+
+    Get in touch for ice cream.
+
+    Kind regards,
+    """,
+
+    """
+    Hi,
+
+    The meeting will be at 3.
+
+    Kind regards,
+    """
+]
+
+TRAINING_LABELS = [
+    1,
+    0
+]
+
+
+NEW_DATA = [
+    """
+    Hi,
+
+    Please find attached the document.
+
+    Regards,
+    """,
+
+    """
+    Hi,
+
+    Would you like to try our ice cream?
+
+    Regards,
+    """
+]
